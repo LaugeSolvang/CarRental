67f6f9bf437395a2b7f7f315fbb6594ee9a61bda
pahol21 <pahj19@gmail.com>
2023-11-24 13:48:58 +0100

    Settings page created
    Doesn't really work as intended tho'

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput, Alert } from 'react-native';
+//import { theme } from '../theme/theme';
 
 const Settings: React.FC = () => {
+  const [text, onChangeText] = React.useState('');
+  const [text1, onChangeText1] = React.useState('');
+  const [text2, onChangeText2] = React.useState('');
+  const notImplemented = () =>  Alert.alert('', 'not implemented')
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Overview Content</Text>
+        <Text style={styles.buttonText}>First Name</Text>
+        <TextInput style={styles.textField} onChangeText={onChangeText} value={text} placeholder='Thomas   '/> 
+        <Text style={styles.buttonText}>Last Name</Text>
+        <TextInput style={styles.textField} onChangeText={onChangeText1} value={text1} placeholder='Johnson  '/>
+        <Text style={styles.buttonText}>Email          </Text> 
+        <TextInput style={styles.textField} onChangeText={onChangeText2} value={text2} placeholder='john@mail'/>
+        <Text/>
+        <Button onPress={notImplemented} title='Change Theme'></Button>
+        <Text/>
+        <Button onPress={notImplemented} title='Log Out'></Button>
+        
       </View>
     );
   }
   
+
+  const styles = StyleSheet.create({
+    textField:{
+      paddingRight: 240,
+      borderWidth:0.5,
+    },
+  
+    buttonText: {
+      marginVertical: 10,
+      paddingRight: 225, 
+    },
+  });
 
 export default Settings;
