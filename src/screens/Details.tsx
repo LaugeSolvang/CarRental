6bee0d4d1ee463d112d97c9628e6c6761831baa9
Miran Sert <miransert@Mirans-MacBook-Air.local>
2023-11-24 12:09:26 +0100

    add

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { CarCard } from '../components/CarCard';
 
-const Details: React.FC = () => {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Overview Content</Text>
-      </View>
-    );
-  }
-  
+const Details = ({ car }) => {
+    console.log('Received car:', car);
+
+  // const IMAGE_URL = `${process.env.EXPO_PUBLIC_IP}${process.env.EXPO_PUBLIC_IMAGE_PORT}/assets/images/${car.pictures[0].srcUrl}`;
+
+  return (
+    <View style={styles.details}>
+      {/* <Image source={{ uri: IMAGE_URL }} style={styles.image} /> */}
+        <Text style={styles.carInfo}>
+          {car.name} - {car.brand}
+        </Text>
+        <Text style={styles.description}>{car.description}</Text>
+        {/* <CarCard /> */}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+    details: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        marginVertical: 5,
+        alignItems: 'stretch', 
+        width: 250,
+        marginRight: 0,
+        marginHorizontal: 8, 
+      },
+      image: {
+        width: '100%',
+        height: 200,
+        borderRadius: 10,
+      },
+      carInfo: {
+        padding: 4,
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 10,
+      },
+      description: {
+        padding: 4,
+        fontSize: 14,
+        color: '#666',
+      },
+      loadingCard: {
+        paddingHorizontal: 15,
+        paddingVertical: 25,
+      },
+      imagePlaceholder: {
+        backgroundColor: '#ddd', // light gray color for the image placeholder
+        height: 200,
+        borderRadius: 10,
+        marginBottom: 10,
+      },
+      textPlaceholder: {
+        height: 20,
+        backgroundColor: '#ddd', // light gray color for the text placeholder
+        marginTop: 10,
+        borderRadius: 5,
+      },
+});
 
 export default Details;
